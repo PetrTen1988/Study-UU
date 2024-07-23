99c535216860f2f03bf49a7d7559405f6f728d52
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-07-23 14:49:53 +0500

    Homework week 1

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+# Практическое задание по уроку "Динамическая типизация"
+name = str('Petr')
+print(name)
+age = int(36)
+print(age)
+New_age = age + 1
+print(New_age)
+Is_student = True
+print(Is_student)
