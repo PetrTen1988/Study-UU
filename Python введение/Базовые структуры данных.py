6379e83080dda46136e385780d421e1e6dc94f50
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-07-28 21:43:59 +0500

    HW 'Строки и индексация строк' added

--- Python введение/Базовые структуры данных.py
+++ Python введение/Базовые структуры данных.py
@@ -1,14 +1,21 @@
-# 1st program
-print(9**0.5*5)
+# # 1st program
+# print(9**0.5*5)
+#
+# # 2nd program
+# print(9.99 > 9.98 and 1000 != 1000.1)
+#
+# # 3rd program
+# num1, num2 = 1234, 5678
+# print((num1 // 10) % 100 + (num2 // 10) % 100)
+#
+# # 4th program
+# a = int(13.42 * 100)
+# b = int(42.13 * 100)
+# print(a // 100 == b % 100 or a % 100 == b // 100)
 
-# 2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
+name = 'is capital'
+a = name.islower()
+print(a)
 
-# 3rd program
-num1, num2 = 1234, 5678
-print((num1 // 10) % 100 + (num2 // 10) % 100)
-
-# 4th program
-a = int(13.42 * 100)
-b = int(42.13 * 100)
-print(a // 100 == b % 100 or a % 100 == b // 100)
+e = 'qwerty'
+print('-'.join(e))
---
+++ Python-разработчик/Строки и индексация строк.py
@@ -0,0 +1,6 @@
+String = 'QweRtyAsd'
+print(String[0])
+print(String[-1])
+print(String[4:])
+print(String[::-1])
+print(String[1::2])
