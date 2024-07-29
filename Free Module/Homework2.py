3657f993fd599fce380b9cdd7773cd1fcfa3c4b3
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-07-29 19:57:21 +0500

    HW 'Динамическая типизация' corrected

--- Free Module/Homework2.py
+++ Free Module/Homework2.py
@@ -1,9 +1,9 @@
 # Практическое задание по уроку "Динамическая типизация"
 name = str('Petr')
-print(name)
+print('Name:', name)
 age = int(36)
-print(age)
+print('Age:',age)
 New_age = age + 1
-print(New_age)
+print('New_Age:', New_age)
 Is_student = True
-print(Is_student)
+print('Is_student:', Is_student)
---
+++ Python-разработчик/Динамическая типизация.py
@@ -0,0 +1,7 @@
+name = str('Petr')
+print('Name:', name)
+age = int(36)
+print('Age:',age)
+print('New_Age:', age + 1)
+Is_student = True
+print('Is_student:', Is_student)
