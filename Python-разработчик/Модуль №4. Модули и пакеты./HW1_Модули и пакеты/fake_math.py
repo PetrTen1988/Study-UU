73a5f557c311d202fcc1e47d2a3ecdd25d415b06
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-08-22 19:06:14 +0500

    HW 'Модули и пакеты' added

---
+++ Python-разработчик/Модуль №4. Модули и пакеты./HW1_Модули и пакеты/fake_math.py
@@ -0,0 +1,8 @@
+# Fake Math module
+
+def divide(first, second):
+    if second != 0:
+        return first/second
+    else:
+        return 'ERROR'
+
---
+++ Python-разработчик/Модуль №4. Модули и пакеты./HW1_Модули и пакеты/true_math.py
@@ -0,0 +1,8 @@
+# True Math module
+import math
+
+def divide(first, second):
+    if second == 0:
+        return math.inf
+    else:
+        return first / second
