49e95b87ad3d5b82697a5cc411f18c0397a00829
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-09-29 17:12:08 +0500

    HW 'Files open modes' added

--- Python-разработчик/Модуль №7. Работа с файлами и форматированный вывод./Files open modes.py
+++ Python-разработчик/Модуль №7. Работа с файлами и форматированный вывод./Files open modes.py
@@ -5,20 +5,38 @@ class Product:
         self.category = category
 
     def __str__(self):
-        print(f'{self.name}, {self.weight}, {self.category}')
+        return f'{self.name}, {self.weight}, {self.category}'
 
 class Shop:
-    __file_name = 'Products.txt'
+    def __init__(self):
+        self.__file_name = 'Products.txt'
+
     def get_products(self):
-        file = file.pprint(name, 'r')
+        file = open(self.__file_name,'r')
+        products = file.read()
+        file.close()
+        return products
+
+    def add(self, *products):
+        current_list = self.get_products()
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if str(product) not in current_list:
+                file.write(str(product) + '\n')
+                current_list += str(product) + '\n'
+            else:
+                print(f'Продукт {product} уже есть в магазине')
+        file.close()
 
 
+s1 = Shop()
 
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
 
+print(p2) # __str__
 
-# p1 = Product('Potato', 50.5, 'Vegetables')
-# p2 = Product('Spaghetti', 3.4, 'Groceries')
-# p3 = Product('Potato', 5.5, 'Vegetables')
-# p1.__str__()
-# p2.__str__()
-# p3.__str__()
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
