cce490b31530980cb8342251c4bc3d99d0293a48
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-10-06 19:44:11 +0500

    HW 'Генераторные сборки' corrected

--- Python-разработчик/Модуль №9. Инструменты функционального программирования./Генераторные сборки.py
+++ Python-разработчик/Модуль №9. Инструменты функционального программирования./Генераторные сборки.py
@@ -1,9 +1,8 @@
-first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбан', 'Компьютер']
+first = ['Strings', 'Student', 'Computers', None]
+second = ['Строка', 'Урбан', 'Компьютер', '123']
 
 first_result = [len(a) - len(b) for a, b in zip(first, second) if len(a) != len(b)]
 second_result = [len(first[i]) == len(second[i]) for i in range(len(first))]
-
 
 print(list(first_result))
 print(list(second_result))
