3366e1740f8a6ce50be5c9c853d1a0992a54d4c0
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-10-07 22:22:14 +0500

    HW 'Файлы в операционной системе' corrected

--- Python-разработчик/Модуль №7. Работа с файлами и форматированный вывод./Файлы в операционной системе.py
+++ Python-разработчик/Модуль №7. Работа с файлами и форматированный вывод./Файлы в операционной системе.py
@@ -1,16 +1,17 @@
 import os
 import time
 
-directory = '/Users/petrten/Documents/GitHub/Study-UU/Python-разработчик/Модуль №7. Работа с файлами и форматированный вывод.'
-for root, dirs, files in os.walk(directory):
+ROOT = os.path.dirname(os.path.abspath(__file__))
+for root, dirs, files in os.walk(ROOT):
     count = 0
     for file in files:
-        file_path = os.path.join(directory,file)
-        file_time = time.ctime(os.path.getctime(file))
+        file_path = os.path.join(ROOT,file)
+        file_time = time.ctime(os.path.getctime(file_path))
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(os.path.getmtime(file)))
-        file_size = os.path.getsize(file)
-        parent_dir = os.path.dirname(directory)
+        file_size = os.path.getsize(file_path)
+        parent_dir = os.path.dirname(file_path)
         count += 1
         print('-------------------------File #', count, '----------------------------------------------')
         print(f'Обнаружен файл: {file}\nПуть: {file_path}\nРазмер: {file_size} байт\nВремя изменения: {formatted_time}\nРодительская директория: {parent_dir}\n')
+        print(f'Всего файлов: {count}')
 
