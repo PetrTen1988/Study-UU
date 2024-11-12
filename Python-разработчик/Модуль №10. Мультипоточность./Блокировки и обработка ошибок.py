886f02fa9282e7ee021f6f9c9b2089460b57fffd
PetrTen1988 <90404768+PetrTen1988@users.noreply.github.com>
2024-11-12 18:58:00 +0500

    HW 'Многопроцессорное программирование' added

--- Python-разработчик/Модуль №10. Мультипоточность./Блокировки и обработка ошибок.py
+++ Python-разработчик/Модуль №10. Мультипоточность./Блокировки и обработка ошибок.py
@@ -1,10 +1,7 @@
 import time
 import threading
-from os import getcwd
 from threading import Lock
-from time import sleep
 from random import randint
-from turtle import TurtleGraphicsError
 
 global balance
 
@@ -12,43 +9,36 @@ class Bank:
     def __init__(self, balance = 0):
         self.balance = balance
         self.lock = threading.Lock()
-        self.depositCounter = 0
-        self.takeCounter = 0
-        self.lockAquired = False
         self.not_on_time_exec = 0
 
 
     def deposit(self):
         for i in range(100):
-            self.lock.acquire()
-            if self.lock.locked() and self.balance >= 0:
+            if self.lock.locked() and self.balance >= 500:
                 self.lock.release()
+
             self.lock.acquire()
             summ = randint(50, 300)
             self.balance += summ
             print(f'Пополнение: {summ}. Баланс: {self.balance}')
-            time.sleep(0.001)
+            time.sleep(0.01)
             self.lock.release()
-
-
-
 
     def take(self):
         for i in range(100):
-            # if self.lock.locked():
-            #     self.lock.release()
-            # self.lock.acquire()
+            self.lock.acquire()
             summ = randint(50, 500)
             print(f'Запрос на снятие {summ}')
             if self.balance >= summ:
                 self.balance -= summ
                 print(f'Снятие: {summ}. Баланс: {self.balance}')
-                time.sleep(0.01)
-                # self.lock.release()
+                time.sleep(0.001)
+                self.lock.release()
             else:
                 print(f'Запрос отклонен, недостаточно средств')
-                time.sleep(0.01)
+                time.sleep(0.001)
                 self.lock.acquire()
+            # self.lock.release()
 
 
 
